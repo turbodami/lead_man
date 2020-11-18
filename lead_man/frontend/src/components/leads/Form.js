5f07c9aba71a7ee2d37eeab9778a6a5fb3ce52b3
Damiano Presa <tolwiz@pop-os.localdomain>
2020-11-18 16:07:43 +0100

    Basic REST & React implemented

---
+++ lead_man/frontend/src/components/leads/Form.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Form extends Component {
+    render() {
+        return (
+            <div>
+                <h1>La vacca che ta caga</h1>
+            </div>
+        )
+    }
+}
+
+export default Form
---
+++ lead_man/frontend/src/components/leads/Leads.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Leads extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Va in cul</h1>
+            </div>
+        )
+    }
+}
+
+export default Leads
